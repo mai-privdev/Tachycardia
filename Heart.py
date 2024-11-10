23c4816ba79db0fc2b2a7ab8613c2de49b4adef4
mai_ <mairealemail@proton.me>
2024-11-10 23:46:38 +0000

    Added homeostasis to heart, and rewrote others

--- Heart.py
+++ Heart.py
@@ -4,14 +4,25 @@ class Heart:
         self.MaxBPM = MaxBPM
         self.CurrentBPM = self.RestBPM
 
-    def heartmove(self):
-        self.CurrentBPM += 5
+    def relax(self):
+        if self.CurrentBPM < 60:
+            self.CurrentBPM += 5
+        else:
+            self.CurrentBPM -= 5
         return(self.CurrentBPM)
 
     def heartscare(self):
+        self.CurrentBPM += 5
+        return(self.CurrentBPM)
+
+    def heartfear(self):
         self.CurrentBPM += 10
         return(self.CurrentBPM)
 
+    def heartterror(self):
+        self.CurrentBPM += 20
+        return(self.CurrentBPM)
+    
     def heartalcohol(self):
         self.CurrentBPM -= 50
         return(self.CurrentBPM)
